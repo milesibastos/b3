4d58df366fe0bd0b501090652f0b669bbf17b194
Antonio Milesi Bastos <antonio@milesibastos.com>
2020-06-07 02:02:00 +0000

    feat: /api/ticker/:ticker

--- api/ticker/[ticker].js
+++ api/ticker/[ticker].js
@@ -2,14 +2,12 @@ const fs = require("fs");
 const path = require("path");
 const { spawn } = require("child_process");
 const unzip = require("unzip-stream");
-const { format } = require("date-fns");
 
 module.exports = (req, res) => {
-  const { ticker, date } = req.query;
-  const dt = format(new Date(date), "yyyyMMdd");
+  const { ticker } = req.query;
   const file = path.resolve("COTAHIST_A2020.ZIP");
 
-  const grep = spawn("grep", ["-E", `${dt}[0-9]{2}${ticker}[[:space:]]+`]);
+  const grep = spawn("grep", ["-E", `[0-9]+${ticker}[[:space:]]+`]);
 
   res.writeHead(200, {
     "Content-Type": "text/event-stream",
